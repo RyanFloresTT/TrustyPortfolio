02b55b66b92081025f6dbeef708d264602293def
Ryan Flores <rryanflorres@gmail.com>
2024-03-26 07:12:40 -0700

    Remove Unity Script, Add DTO for Tag

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,10 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TrustyPortfolio.Data;
+using TrustyPortfolio.Models.Domain;
+using TrustyPortfolio.Models.ViewModels;
 
 namespace TrustyPortfolio.Controllers {
     public class AdminTagsController : Controller {
+        readonly PortfolioDbContext db;
+        public AdminTagsController(PortfolioDbContext dbContext) {
+            db = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add() {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]    
+        public IActionResult Add(AddTagRequest tagRequest) {
+
+            var tag = new Tag {
+                Name = tagRequest.Name,
+                DisplayName = tagRequest.DisplayName
+            };
+
+            db.Tags.Add(tag);
+            db.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List() {
+            
+            var tags = db.Tags.ToList();
+
+            return View(); 
         }
     }
 }
---
+++ Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace TrustyPortfolio.Models.ViewModels {
+    public class AddTagRequest {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
