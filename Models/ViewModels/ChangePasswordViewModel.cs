44d0615641b30c027baaa13bd3513388ece08f82
Ryan Flores <53247675+RyanFloresTT@users.noreply.github.com>
2024-03-29 22:34:25 -0700

    Remove Seeded, Changed Password

---
+++ Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TrustyPortfolio.Models.ViewModels {
+    public class ChangePasswordViewModel {
+        public string Username { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
