a82b4593d41619b7a7d5d8fe07c930cf76ff7adb
Ryan Flores <53247675+RyanFloresTT@users.noreply.github.com>
2024-05-02 09:56:50 -0700

    Clean Up from AspNet Project

---
+++ Components/Pages/Admin/TagCreate.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using TrustyPortfolio.Models.Domain;
+using TrustyPortfolio.Repositories;
+
+namespace TrustyPortfolio.Components.Pages.Admin {
+    public partial class TagCreate {
+        [Inject] NavigationManager NavigationManager { get; set; }
+        [Inject] ITagRepository TagRepository { get; set; }
+        public Tag? Tag { get; set; }
+
+        private async Task HandleSubmit() {
+            try {
+                await TagRepository.AddAsync(Tag);
+                NavigationManager.NavigateTo("/Admin/Tags/Edit");
+            }
+            catch (Exception ex) {
+                // Handle any errors during update (show error message)
+            }
+        }
+
+    }
+}
--- Controllers/AboutController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace TrustyPortfolio.Controllers {
-    public class AboutController : Controller {
-        public IActionResult Index() {
-            return View();
-        }
-    }
-}
