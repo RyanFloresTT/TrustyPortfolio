c6b507f2d332f95472df0930de8459e9d41845ce
Ryan Flores <53247675+RyanFloresTT@users.noreply.github.com>
2024-05-12 13:16:53 -0700

    highlight project code & add other languages

--- Components/Components/ProjectIndex.razor.cs
+++ Components/Components/ProjectIndex.razor.cs
@@ -1,10 +1,12 @@
 ﻿using Markdig;
 using Markdig.Prism;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using TrustyPortfolio.Models.Domain;
 
 namespace TrustyPortfolio.Components.Components {
     public partial class ProjectIndex {
+        [Inject] IJSRuntime JS { get; set; }
         [CascadingParameter] PortfolioData PortfolioData { get; set; }
         [Parameter] public string UrlHandle { get; set; }
         public Project? Project { get; set; }
@@ -13,6 +15,9 @@ namespace TrustyPortfolio.Components.Components {
             await base.OnParametersSetAsync();
             Project = PortfolioData.Projects.FirstOrDefault(x => x.UrlHandle == UrlHandle);
 
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender) {
+            await JS.InvokeVoidAsync("highlightAll");
         }
 
         string GetBlogUrlHandle(string blogUrlHandle) => $"/Blogs/{blogUrlHandle}";
